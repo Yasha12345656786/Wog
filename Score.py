c099b5ed37e2a360b797366ed0f36b22f3b04ad6
yasha <jacob210usu@gmail.com>
2024-05-19 13:58:20 +0300

    started doing part 3 of the project

---
+++ Score.py
@@ -0,0 +1,8 @@
+def points_of_winning(difficulty):
+    points = (difficulty*3)+5
+    return points
+
+def add_score(difficulty):
+
+
+
---
+++ Utils.py
@@ -0,0 +1,8 @@
+# SCORES_FILE_NAME
+import os
+
+BAD_RETURN_CODE = -1
+
+
+def screen_cleaner():
+    os.system('cls')
