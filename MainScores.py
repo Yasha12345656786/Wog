8109b93909f1d22bb60e1a355565fec5d47a2459
yasha <jacob210usu@gmail.com>
2024-05-30 19:45:04 +0300

    added embed tag in html for the score part still need to add the error and ask if i can name a new file.txt as one i used before (Scores.txt)

--- MainScores.py
+++ MainScores.py
@@ -1,18 +1,25 @@
 from flask import render_template, Flask
-
+import os
 
 def score_server():
     try:
-        app = Flask(__name__, template_folder='templates/')
-
+        app = Flask(__name__, template_folder='./html')
         @app.route("/")
         def players_points():
-            return render_template('PlayersPoints.html')
+            scores = []
+            with open("Scores.txt") as f:
+                scores = [line.rstrip('\n') for line in f]
+            return render_template('PlayersPoints.html', scores=scores)
 
         app.run(debug=True)
     except Exception as e:
-        app = Flask(__name__, template_folder='templates/')
+        app = Flask(__name__, template_folder='./html')
 
         @app.route("/")
         def players_points():
             return render_template('PlayersPointsErr.html')
+
+        app.run(debug=True)
+
+
+score_server()
--- Score.py
+++ Score.py
@@ -1,15 +1,20 @@
+global file
+
+
 def add_score(difficulty):
+    global file
     points_of_winning = (difficulty * 3) + 5
     try:
         file = open('Scores.txt', 'r+')
+    except FileNotFoundError:
+        file = open('Scores.txt', 'a')
+    finally:
         data = file.read()
         print(f"Your Score:{data}")
         file = open('Scores.txt', 'a')
-        file.write(f"{points_of_winning}")
+        file.write(f"{points_of_winning}\n")
         file.close()
         print("Your Score Was Saved")
-    except FileNotFoundError:
-        file1 = open('Scores.txt', 'a')
-        file1.write(f"{points_of_winning}")
-        file1.close()
-        print("Your Score Was Saved")
+
+
+add_score(3)
