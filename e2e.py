84998c5f6c33c7d04a84da63a2a5024aca5079e8
yasha <jacob210usu@gmail.com>
2024-06-02 19:46:43 +0300

    added embed tag in html for the score part still need to add the error and ask if i can name a new file.txt as one i used before (Scores.txt)

--- e2e.py
+++ e2e.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
 from time import sleep
 
-try:
+
+def test_score_service():
     dr = webdriver.Chrome()
-    dr.get("http://127.0.0.1:5000")
-    score = dr.find_element(by="id", value="score")
-    if int(score) >= 1 or int(score) <= 1000:
-        print("true")
-except Exception as error:
-    print("some error", error)
+    current_score=0
+    try:
+        dr.get("http://127.0.0.1:5000")
+        score_obj = dr.find_element(by="id", value="score")
+        txt = score_obj.text
+        scores_string_arr = txt.split("\n")
+        for item in scores_string_arr:
+            score = int(item.split(": ")[-1])
+            current_score = score
+            if current_score < 1 or current_score > 1000:
+                assert False
+    except AssertionError as score:
+        print(f"The Score {current_score} Is Invalid")
+    finally:
+        dr.close()
+
+
+test_score_service()
